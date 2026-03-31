61c116176679dc6dafd557c31cf33542813a0351
sarathdevopsm <130728819+sarathdevopsm@users.noreply.github.com>
2026-03-31 13:09:29 +0530

    hello world started

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Trump
+PERSON2=Musk
+
+echo "$PERSON1: Hey $PERSON2, How are you"
+echo "$PERSON2: : Hello $PERSON1, I'm good! thanks.. How about you"
+echo "$PERSON1: Doing fine man! whats up"
+echo "$PERSON2: :Nothing! going to allagadda to meet Sarath"
+echo "$PERSON1: such a good man Sarath"
