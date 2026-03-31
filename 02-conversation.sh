21b61b4c9fe2f4d6de00dfaf6da1864b53015fd5
sarathdevopsm <130728819+sarathdevopsm@users.noreply.github.com>
2026-03-31 13:03:09 +0530

    hello world started

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Trump: Hey Mush, How are you"
+echo "Musk: : Hello Trump, I'm good! thanks.. How about you"
+echo "Trump: Doing fine man! whats up"
+echo "Musk: :Nothing! going to allagadda to meet Sarath"
+echo "Trump: such a good man Sarath"
