cfcba774c79267a3097518aa2cce1e8f369a964d
sarathdevopsm <130728819+sarathdevopsm@users.noreply.github.com>
2026-04-02 05:19:22 +0530

    first commit

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo "Trump: Hey Mush, How are you"
-echo "Musk: : Hello Trump, I'm good! thanks.. How about you"
-echo "Trump: Doing fine man! whats up"
-echo "Musk: :Nothing! going to allagadda to meet Sarath"
-echo "Trump: such a good man Sarath"
+echo "Sarath:Hey Jagadeesh how are you?"
+echo "Jagadeesh: Hey Sarath whats up whatsup my rowdy babies"
+echo "Sarath: konchm over ayndi"
+echo "Jagadeesh: idi common bro"
