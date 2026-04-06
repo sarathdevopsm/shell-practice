76ed9c8ee7e4a4a72ea6b731d89a05390bae52dc
sarathdevopsm <130728819+sarathdevopsm@users.noreply.github.com>
2026-04-06 13:47:00 +0530

    hello world started

--- 08-special_variables.sh
+++ 08-special_variables.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "all variables passed to the script: $@"
+echo "Number of variables"$#"
+echo "script name: $0"
+echo "current Directory: $PWD"
+echo "User running this script: $User"
+echo "Home directory of user: $Home"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of last command in backround"$!"
