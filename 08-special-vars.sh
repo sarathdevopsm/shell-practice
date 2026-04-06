51e1df7007eb4fc29b5144a9bcaf4a2e650dcdab
sarathdevopsm <130728819+sarathdevopsm@users.noreply.github.com>
2026-04-06 14:10:08 +0530

    hello world started

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -8,4 +8,4 @@ echo "User running this script: $USER"
 echo "Home directory of user: $HOME"
 echo "PID of the script: $$"
 sleep 10 &
-echo "PID of last command in backround: "$!"
+echo "PID of last command in backround:$!"
