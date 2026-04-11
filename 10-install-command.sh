58b29fbefd155beece4cef4a1dc77941c40c0756
sarathdevopsm <130728819+sarathdevopsm@users.noreply.github.com>
2026-04-11 18:00:07 +0530

    hello world started

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -5,6 +5,9 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "ERROR": :please run with root access]
+    exit 1 #give other than 0 upto 127
 else
      echo " You are running with root access"
 fi
+
+dnf install mysql -y
